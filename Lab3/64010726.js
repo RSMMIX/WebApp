80da3d0a1f1f50973fe319b7e62634fc3735bde1
Rattanaporn <88606135+RSMMIX@users.noreply.github.com>
2023-02-15 04:30:08 +0700

    Ex4

---
+++ Lab3/64010726.js
@@ -0,0 +1,9 @@
+self.onmessage =(message)=>{
+    console.log("SDfas")
+    let send_time_now = () => {
+        postMessage(new Date().toString());
+    };
+    postMessage(new Date().toString());
+    setInterval(send_time_now, 1000);
+    // console.log("test ewdqwedqf -> "+e)
+}
