44bd755101ae39d85e9d0ffa8164159693da20dc
Rattanaporn <88606135+RSMMIX@users.noreply.github.com>
2023-02-22 01:48:22 +0700

    Ex5

---
+++ react-web/src/App.js
@@ -0,0 +1,17 @@
+import Header from './Header';
+import Card from './components/Card';
+import Vote from './components/Vote';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Card />
+      <Vote />
+    </div>
+  );
+}
+
+
+export default App;
---
+++ react-web/src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Header =() =>{
+    return(         
+    <div className="Txet-Header">
+          <h1>โหวตอาหาร</h1>
+    </div>
+    );
+}
+
+export default Header;
